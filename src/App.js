3cf0dc2af176d2e9e05a273b4b417bef71d020c6
ali <ali.marinas@icloud.com>
2020-11-18 11:56:34 -0800

    continue with header and added hero components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Header from './components/Header';
 import Home from './containers/Home';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Hero />
       <Home />
       
     </div>
---
+++ src/components/Hero/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'
+
+const Hero = (props) => {
+return(
+  <div>
+     Hero
+  </div>
+)
+}
+
+export default Hero
