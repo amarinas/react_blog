93b0c6ce009116fedc2f95a10bbcc35b32588e5d
ali <ali.marinas@icloud.com>
2020-11-22 21:29:43 -0800

    added images to home

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -14,13 +14,14 @@ const SideBar = (props) => {
 
 return(
 
-  <div className="sidebarContainer" >
+  <div className="sidebarContainer"  >
+
   <Card style={{marginBottom: '20px', padding: '20px', boxSizing:'border-box'}}>
    <div className="cardHeader">
      <span>About Us</span>
     </div>
     <div className="profileImageContainer">
-    <img src={'https://images.unsplash.com/photo-1549991191-5362425682ee?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'} alt="no image found" />
+    <img src={'https://images.unsplash.com/photo-1549991191-5362425682ee?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'} alt="no photo" />
     </div>
 
   <div className="cardBody">
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,45 @@
 import React from 'react';
+
+
+import Card from '../../components/UI/Card';
+
 import './style.css';
 
 
 const Home = props => {
+  const galleryHeight = 450;
+  const galleryStyle = {
+    height: galleryHeight+'px',
+    overflow: 'hidden'
+  }
+
+  const sideImage = galleryHeight / 3;
+
   return(
     <div>
-      Home
+      <Card>
+        <div className="galleryPost" style={{galleryStyle}}>
+        <section style={{width:'70%'}} >
+          <div >
+            <img src="https://images.unsplash.com/photo-1605812276723-c31bb1a68285?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt ="not found"></img>
+          </div>
+          
+        </section>
+
+        <section className="sideImageWrapper" style={{width:'30%'}}>
+          <div sytle={{height: `${sideImage}px`}}>
+            <img src="https://images.unsplash.com/photo-1605812276723-c31bb1a68285?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt ="not found"></img>
+          </div>
+          <div sytle={{height: `${sideImage}px`}}>
+            <img src="https://images.unsplash.com/photo-1605812276723-c31bb1a68285?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt ="not found"></img>
+          </div>
+          <div sytle={{height: `${sideImage}px`}}>
+            <img src="https://images.unsplash.com/photo-1605812276723-c31bb1a68285?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" alt ="not found"></img>
+          </div>
+        </section>         
+        </div>
+
+      </Card>
     </div>
   );
 }
